209b79a02e95df62347ef640736d0d807d2f7224
jsm33t <jskainthofficial@gmail.com>
2024-09-04 23:38:44 +0530

    env change

--- src/ui/src/environment/environment.prod.ts
+++ src/ui/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.jassi.me',
+    apiUrl: 'https://api.jsm33t.me',
     cdnUrl:'https://almondcove.in',
     loaderWait: 2500
 };
